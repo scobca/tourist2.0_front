cdbcc8c6056743aed63911431a026fb6431af153
scobca <scobca18@yandex.ru>
2023-06-18 02:08:34 +0300

    Creating main.css and some settings
    1) Adding styles directory to assets
    2) Importing main.css to project
    3) Creating :root parameters in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <v-header/>
+    <router-view/>
 </template>
 
+
+<script>
+import VHeader from "@/components/vHeader.vue";
+
+export default {
+    name: 'app',
+    components: {VHeader}
+}
+</script>
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+:root {
+    --text-color-light: hsl(210, 29%, 24%);
+    --text-color-dark: hsl(208, 50%, 54%);
+    --border-color-hold: hsl(153, 47%, 49%);
 }
 </style>
