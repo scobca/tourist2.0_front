7d30dec7c8768208532d8b419b36ae67bb9d96d1
scobca <scobca18@yandex.ru>
2023-06-18 21:17:09 +0300

    Base layout for header

--- src/components/vHeader.vue
+++ src/components/vHeader.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <div class="logo_container">
-
+            <h1 class="logo">Tourist.ru</h1>
         </div>
         <div class="buttons_container">
             <ul class="buttons_list">
@@ -29,26 +29,51 @@ export default {
     display: flex;
     flex-direction: row;
     justify-content: space-between;
-    margin: .5rem 1rem .5rem 1rem;
+    margin: 0;
+    padding: 0 .5rem;
 
     height: 3.5rem;
-    background: transparent;
+    background: var(--text-color-dark);
     //border: 3px black solid;
 }
 
 .logo_container, .buttons_container {
-    min-width: 30rem;
-    border: 2px solid red;
+    display: flex;
+    flex-direction: row;
+    justify-content: left;
+    align-items: center;
+    max-width: 40rem;
+    //border: 2px solid red;
+
 }
 
 .buttons_list {
     display: flex;
     flex-direction: row-reverse;
-    justify-content: flex-start;
-    align-content: center;
-    margin: 1rem;
+    gap: 3rem;
+    list-style: none;
+    cursor: pointer;
+    //border: 2px solid green;
+    min-height: 2rem;
+}
 
-    gap: 2rem;
-    border: 2px solid red;
+.button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 9rem;
+    height: 2rem;
+
+    border: 2px solid purple;
+    border-radius: .5rem;
+
+    background: lightblue;
+}
+
+.logo {
+    font-family: Allison, sans-serif;
+    font-size: 4rem;
+    font-weight: lighter;
 }
 </style>
